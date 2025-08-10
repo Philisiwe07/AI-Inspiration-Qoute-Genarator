91730c3dca23386c4aa4fcc5e3e030936c135ae5
Philisiwe07 <123089991+Philisiwe07@users.noreply.github.com>
2025-08-10 15:25:44 +0200

    added axios in html and added some javascripts funvtions

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-function generatePoem(event) {
-    event.preventDefault();
-    new Typewriter('#poem', {
-  strings: "I dreamed you called without reason, picking up where we left" ,
+function displayPoem(response){
+     new Typewriter('#poem', {
+  strings: response.data.answer,
   autoStart: true,
   delay: 1,
   cursor: "",
 });
 
     
-      
+
 }
 
 
+
+
+function generatePoem(event) {
+    event.preventDefault();
+   
+let apiKey = "t0033d45f1b2356ec4oc18a3a5f2a16d";
+let prompt = "generate a poem about what is typed in the input field";
+let context = "you are a poet, write a poem based on the prompt given";
+let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+
+axios.get(apiURL).then(displayPoem);
+console.log(response);
+
+      
+}
 
 
 
